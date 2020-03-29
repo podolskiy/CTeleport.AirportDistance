90108b6ad472049fe22c95283597b8f93ac7999c
podolskiy <anthony.podolskiy@gmail.com>
2020-03-29 15:02:12 +0300

    Update AirportControllerTest.cs

--- CTeleport.AirportDistance.Tests/AirportControllerTest.cs
+++ CTeleport.AirportDistance.Tests/AirportControllerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using CTeleport.AirportDistance.Api.Controllers;
 using CTeleport.AirportDistance.Api.Models;
@@ -13,21 +14,60 @@ namespace CTeleport.AirportDistance.Tests
 {
     public class AirportControllerTest
     {
-        private readonly AirportController _controller;
-        public AirportControllerTest()
-        {
-            var mockRepo = new Mock<IAirportService>();
-            mockRepo.Setup(repo => repo.CalculateDistance(It.IsAny<DistanceModel>())).ReturnsAsync(It.IsAny<double>);
-
-            _controller = new AirportController(new Lazy<IAirportService>(mockRepo.Object));
-        }
 
         [Fact]
         public async Task DistanceGetShouldHaveValidResult()
         {
-            var result = await _controller.Distance(It.IsAny<DistanceRequest>());
+            // Arrange
+            var mockRepo = new Mock<IAirportService>();
+            mockRepo
+                .Setup(repo => repo.CalculateDistance(It.IsAny<DistanceModel>()))
+                .ReturnsAsync(It.IsAny<double>);
 
+            var controller = new AirportController(new Lazy<IAirportService>(mockRepo.Object));
+
+            // Act
+            var result = await controller.Distance(It.IsAny<DistanceRequest>());
+
+            // Assert
             Assert.IsAssignableFrom<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Distance_ReturnsCompleteViewModel()
+        {
+            // Arrange
+            var miles = (double) new Random().Next(0, Int32.MaxValue);
+            var mockRepo = new Mock<IAirportService>();
+            mockRepo
+                .Setup(repo => repo.CalculateDistance(It.IsAny<DistanceModel>()))
+                .ReturnsAsync(miles);
+            var controller = new AirportController(new Lazy<IAirportService>(mockRepo.Object));
+
+            // Act
+            var actionResult = await controller.Distance(It.IsAny<DistanceRequest>());
+            var model = (actionResult as OkObjectResult)?.Value as double?;
+
+            // Assert
+            Assert.Equal(model, miles);
+        }
+
+        [Fact]
+        public async Task DistanceAction_RepositoryCalledOnce()
+        {
+            // Arrange
+            var mockRepo = new Mock<IAirportService>();
+            mockRepo
+                .Setup(repo => repo.CalculateDistance(It.IsAny<DistanceModel>()))
+                .ReturnsAsync(It.IsAny<double>);
+
+            var controller = new AirportController(new Lazy<IAirportService>(mockRepo.Object));
+
+            // Act
+            var actionResult = await controller.Distance(It.IsAny<DistanceRequest>());
+
+            // Assert
+            mockRepo.Verify(_ => _.CalculateDistance(It.IsAny<DistanceModel>()), Times.Once);
         }
     }
 }
